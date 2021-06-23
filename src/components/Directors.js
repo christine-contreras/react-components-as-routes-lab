72a8fee6931db426b5a6b98603ceab53b894a878
Christine Contreras <christineec23@yahoo.com>
2021-06-23 11:56:23 -0500

    commit tree e422f84a6cb1dcd271a1555f15225870c9a57289

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { directors } from '../data';
 
+const renderDirectors = () => {
+  return directors.map(director => {
+    return (
+      <div className="director" key={director.name}>
+        <h2 className="h3">{director.name}</h2>
+        <p>Movies:</p>
+        <ul>
+          {director.movies.map(movie => <li>{movie}</li>)}
+        </ul>
+      </div>
+    )
+      
+  })
+}
+
+
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+        {renderDirectors()}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { movies } from '../data';
 
+const renderMovies = () => {
+  return movies.map(movie => {
+    return (
+      <div className="movie" key={movie.title}>
+        <h2 className="h3">{movie.title}</h2>
+        <p>Movie Time: {movie.time} min.</p>
+        <ul>
+          {movie.genres.map(genre => <li>{genre}</li>)}
+        </ul>
+      </div>
+    )
+      
+  })
+}
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {renderMovies()}
     </div>
   );
 };
